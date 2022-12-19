5d16c3428aece3311a68cecfafb858188cd35437
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-12-18 20:35:45 -0500

    General Refactoring

--- src/oniguruma.js
+++ src/oniguruma.js
@@ -5,6 +5,9 @@ const { OnigScanner } = require('../build/Release/onig_scanner.node')
 const { OnigString } = require('../build/Release/onig_scanner.node')
 
 
+const { max } = Math;
+
+
 class OnigRegExp {
 
 	constructor ( source ){
@@ -89,56 +92,99 @@ class OnigRegExp {
 
 
 
-OnigScanner.prototype.findNextMatch = function (string, startPosition, callback) {
-  if (startPosition == null) startPosition = 0
-  if (typeof startPosition === 'function') {
-    callback = startPosition
-    startPosition = 0
-  }
+OnigScanner.prototype.findNextMatch =
 
-  string = this.convertToString(string)
-  startPosition = this.convertToNumber(startPosition)
+	function ( string , startPosition = 0 , callback ){
 
-  this._findNextMatch(string, startPosition, (error, match) => {
-    if (match) match.scanner = this
-    return callback(error, match)
-  })
-}
+		if(typeof startPosition === 'function'){
+			callback = startPosition
+			startPosition = 0
+		}
 
-OnigScanner.prototype.findNextMatchSync = function (string, startPosition) {
-  if (startPosition == null) { startPosition = 0 }
-  string = this.convertToString(string)
-  startPosition = this.convertToNumber(startPosition)
+		startPosition = this.convertToNumber(startPosition);
+		string = this.convertToString(string);
 
-  let match = this._findNextMatchSync(string, startPosition)
-  if (match) match.scanner = this
-  return match
-}
+		const onMatch = ( error , match ) => {
 
-OnigScanner.prototype.convertToString = function (value) {
-  if (value === undefined) return 'undefined'
-  if (value === null) return 'null'
-  if (value.constructor == OnigString) return value
-  return value.toString()
-}
+			if(match)
+				match.scanner = this;
 
-OnigScanner.prototype.convertToNumber = function (value) {
-  value = parseInt(value)
-  if (!isFinite(value)) { value = 0 }
-  value = Math.max(value, 0)
-  return value
-}
+			return callback(error,match)
+		}
 
-OnigString.prototype.substring = function (start, end) {
-  return this.content.substring(start, end)
-}
+		this._findNextMatch(string,startPosition,onMatch);
+	}
 
-OnigString.prototype.toString = function (start, end) {
-  return this.content
-}
 
-Object.defineProperty(OnigString.prototype, 'length', {
-  get() { return this.content.length }
+OnigScanner.prototype.findNextMatchSync =
+
+	function ( string , startPosition = 0 ){
+
+		startPosition = this.convertToNumber(startPosition);
+		string = this.convertToString(string);
+
+		const match = this._findNextMatchSync(string,startPosition);
+
+		if( match )
+			match.scanner = this;
+
+		return match
+	}
+
+
+OnigScanner.prototype.convertToString =
+
+	function ( value ){
+
+		if( value === undefined )
+			return 'undefined'
+
+		if( value === null )
+			return 'null'
+
+		if( value.constructor == OnigString )
+			return value
+
+		return value
+			.toString()
+	}
+
+
+OnigScanner.prototype.convertToNumber =
+
+	function ( value ){
+
+		value = parseInt(value)
+
+		if( ! isFinite(value) )
+			value = 0;
+
+		value = max(value,0)
+
+		return value
+	}
+
+
+OnigString.prototype.substring =
+
+	function ( start , end ) {
+		return this.content
+			.substring(start,end)
+	}
+
+
+OnigString.prototype.toString =
+
+	function ( start , end ){
+		return this.content
+	}
+
+
+Object.defineProperty(OnigString.prototype,'length',{
+
+	get (){
+		return this.content.length
+	}
 })
 
 exports.OnigScanner = OnigScanner
