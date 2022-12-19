7816781606965f58163db5b96b8cbc40704ef1a9
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-12-18 23:53:25 -0500

    Refactored Scanner Worker

--- src/onig-scanner-worker.hpp
+++ src/onig-scanner-worker.hpp
@@ -1,5 +1,5 @@
-#ifndef SRC_ONIG_SCANNER_WORKER_H_
-#define SRC_ONIG_SCANNER_WORKER_H_
+#ifndef Header_Oni_Scanner_Worker
+#define Header_Oni_Scanner_Worker
 
 #include <memory>
 #include <vector>
@@ -11,30 +11,43 @@
 using ::std::shared_ptr;
 using ::std::vector;
 
+
 class OnigScannerWorker : public Nan::AsyncWorker {
- public:
-  OnigScannerWorker(Nan::Callback *callback,
-                    vector<shared_ptr<OnigRegExp>> regExps,
-                    Local<String> v8String,
-                    int charOffset)
-    : Nan::AsyncWorker(callback),
-      charOffset(charOffset) {
-        source = new OnigString(v8String);
-    searcher = shared_ptr<OnigSearcher>(new OnigSearcher(regExps));
-  }
 
-  ~OnigScannerWorker() {
-    delete source;
-  }
+	private:
 
-  void Execute();
-  void HandleOKCallback();
+		shared_ptr<OnigSearcher> searcher;
+		shared_ptr<OnigResult> bestResult;
 
- private:
-  OnigString* source;
-  int charOffset;
-  shared_ptr<OnigSearcher> searcher;
-  shared_ptr<OnigResult> bestResult;
+		OnigString * source;
+
+		int charOffset;
+
+
+	public:
+
+		OnigScannerWorker (
+			Nan::Callback * callback,
+			vector<shared_ptr<OnigRegExp>> regExps,
+			Local<String> v8String,
+			int charOffset
+		) : Nan::AsyncWorker(callback)
+		  , charOffset(charOffset) {
+
+			source = new OnigString(v8String);
+
+			searcher = shared_ptr<OnigSearcher>
+				(new OnigSearcher(regExps));
+		}
+
+		~OnigScannerWorker (){
+			delete source;
+		}
+
+		void HandleOKCallback();
+		void Execute();
+
 };
 
-#endif  // SRC_ONIG_SCANNER_WORKER_H_
+
+#endif
